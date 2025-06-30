c98cf2b336e61e0118b4bad5cf9a357d70b4562a
Franklyn Viana dos Santos <franklyn_vs_@hotmail.com>
2025-06-29 21:51:13 -0300

    Adds pages for logging in, registering, editing and deleting notes, as well as improving the display of notes and updating the style of the logout button

--- resources/views/top_bar.blade.php
+++ resources/views/top_bar.blade.php
@@ -10,7 +10,7 @@
     <div class="col">
         <div class="d-flex justify-content-end align-items-center">
             <span class="me-3"><i class="fa-solid fa-user-circle fa-lg text-secondary me-3"></i>{{ session('user.username') }}</span>
-            <a href="{{ route('logout') }}" class="btn btn-outline-secondary px-3">
+            <a href="{{ route('logout') }}" class="btn btn-outline-primary px-3">
                 Logout<i class="fa-solid fa-arrow-right-from-bracket ms-2"></i>
             </a>
         </div>
